b5dc9a57080aaa1ed8c69ec5eb93814304c7fa13
Jayram Bagal <94613732+jayrambagal@users.noreply.github.com>
2022-12-15 10:50:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,132 @@
+const calculator = document.querySelector('.calculator');
+const display = document.querySelector('.calculator__display');
+const keys = document.querySelector('.calculator__keys');
+
+
+keys.addEventListener('click',e =>{
+    if (e.target.matches('button')){
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+
+        const calculate = (n1, operator, n2) => {
+            let result = ''
+            
+            if (operator === 'add') {
+              result = parseFloat(n1) + parseFloat(n2)
+            } else if (operator === 'subtract') {
+              result = parseFloat(n1) - parseFloat(n2)
+            } else if (operator === 'multiply') {
+              result = parseFloat(n1) * parseFloat(n2)
+            } else if (operator === 'divide') {
+              result = parseFloat(n1) / parseFloat(n2)
+            }
+            
+            return result
+          }
+
+        
+        Array.from(key.parentNode.children)
+        .forEach(k => k.classList.remove('is-depressed'))
+
+        // for clicking numbers they display in disply box div
+        if (!action) {
+            if (
+              displayedNum === '0' ||
+              previousKeyType === 'operator' ||
+              previousKeyType === 'calculate'
+            ) {
+              display.textContent = keyContent
+            } else {
+              display.textContent = displayedNum + keyContent
+            }
+            calculator.dataset.previousKeyType = 'number'
+          }
+          
+          if (action === 'decimal') {
+            if (!displayedNum.includes('.')) {
+              display.textContent = displayedNum + '.'
+            } else if (
+              previousKeyType === 'operator' ||
+              previousKeyType === 'calculate'
+            ) {
+              display.textContent = '0.'
+            }
+            
+          calculator.dataset.previousKeyType = 'decimal'
+          }
+
+        
+        // click the operators and display on display box
+        // when you click the any oparator then you can type the second number
+        if (
+            action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide')
+             {
+
+
+                const firstValue = calculator.dataset.firstValue
+                const operator = calculator.dataset.operator
+                const secondValue = displayedNum
+                
+                // Note: It's sufficient to check for firstValue and operator because secondValue always exists
+                if (firstValue && operator && previousKeyType !== 'operator') {
+                    display.textContent = calculate(firstValue, operator, secondValue)
+                    display.textContent = calcValue
+                    // updtaing the new result value to first value
+                    calculator.dataset.firstValue = calcValue
+                }
+                else{
+                    calculator.dataset.firstValue = displayedNum
+                }
+                key.classList.add('is-depressed')
+                
+                calculator.dataset.previousKeyType = 'operator'
+                calculator.dataset.operator = action
+
+            
+          }
+
+        //   Calculating the values
+        if (action === 'calculate') {
+            let firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+            
+          if (firstValue) {
+              if (previousKeyType === 'calculate') {
+                firstValue = displayedNum
+                secondValue = calculator.dataset.modValue
+              }
+              
+          display.textContent = calculate(firstValue, operator, secondValue)
+            }
+            
+          // Set modValue attribute
+            calculator.dataset.modValue = secondValue
+            calculator.dataset.previousKeyType = 'calculate'
+          }
+
+
+        //   clearing the screen and set all values to zeros
+          if (action === 'clear') {
+            if (key.textContent === 'AC') {
+              calculator.dataset.firstValue = ''
+              calculator.dataset.modValue = ''
+              calculator.dataset.operator = ''
+              calculator.dataset.previousKeyType = ''
+            } else {
+              key.textContent = 'AC'
+            }
+            
+            display.textContent = 0
+            calculator.dataset.previousKeyType = 'clear'
+          }
+
+
+        
+        
+
+    }
+})
